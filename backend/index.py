e6475a32d93997b4b60841b6c00f8a65ccebe2b1
somni <me@somni.one>
2021-11-13 19:33:04 +0900

    Add mollufying server part

--- backend/index.py
+++ backend/index.py
@@ -1,7 +1,17 @@
-from mollufy import mollufy
+from flask import Flask, request, jsonify
+from mollufy import mollufy as mollufy_internal
 
-def main():
-  print(mollufy(input().strip()))
+server = Flask("mollufier")
+
+@server.route("/mollufy", methods=["POST"])
+def mollufy():
+  print(request.get_json())
+
+  if request.is_json:
+    text = request.get_json()["sentence"]
+    
+    responseData = { "content": mollufy_internal(text) }
+    return jsonify(responseData)
 
 if __name__ == "__main__":
-  main()
+  server.run()
