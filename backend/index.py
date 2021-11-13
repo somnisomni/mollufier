1107cbaf951ba266829173c399ee4ea871816421
somni <me@somni.one>
2021-11-13 18:50:19 +0900

    Make mollufing sentence work as expected

---
+++ backend/index.py
@@ -0,0 +1,7 @@
+from mollufy import mollufy
+
+def main():
+  print(mollufy(input().strip()))
+
+if __name__ == "__main__":
+  main()
---
+++ backend/mollufy.py
@@ -0,0 +1,24 @@
+from kiwipiepy import Kiwi
+kiwi = Kiwi()
+
+def mollufy(sentence):
+  analyzed = kiwi.analyze(sentence)
+  mollufiedSentence = ""
+
+  offset = 0
+
+  for token in analyzed[0][0]:
+    if offset < token.start:
+      mollufiedSentence += sentence[offset:token.start]
+    
+    if token.tag.startswith("N") and token.len == 2:
+      mollufiedSentence += "{}?{}".format(sentence[token.start:token.start + 1], sentence[token.start + 1:token.start + 2])
+    else:
+      mollufiedSentence += sentence[token.start:token.start + token.len]
+    
+    offset = token.start + token.len
+  
+  if offset < len(sentence):
+    mollufiedSentence += sentence[offset:]
+
+  return mollufiedSentence
