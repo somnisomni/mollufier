3adc1440b6a44d26878996a4d5bc806889facef5
somni <me@somni.one>
2021-11-13 19:46:52 +0900

    Setup empty Vue frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class App extends Vue {}
+</script>
---
+++ frontend/vue.config.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+module.exports = require("@vue/cli-service").defineConfig({
+  transpileDependencies: true,
+});
