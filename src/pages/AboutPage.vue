15b0be91bb86ad0e37b3db3295e79de83a40ea60
somni <me@somni.one>
2021-11-18 00:33:17 +0900

    Add author and link of used (unofficial) BlueArchive stickers

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -3,6 +3,7 @@
     <h1>몰?루파이어 <small>v{{ $store.state.appVersion }}</small></h1>
     <p>준비중입니당!</p>
     <p>Made by <a href="https://twitter.com/somni_somni" target="_blank">somni (@somni_somni)</a></p>
+    <p><a href="https://www.pixiv.net/artworks/89423246">블루아카콘</a> by <a href="https://www.pixiv.net/users/64509815" target="_blank">coconutcorn</a></p>
   </div>
 </template>
 
