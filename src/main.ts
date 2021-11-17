33b16126ceb2eebebd9745f885f38b7b44a51002
somni <me@somni.one>
2021-11-18 00:03:20 +0900

    Add `vuex`

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
+import router from "@/plugins/router";
+import store from "@/plugins/store";
 
 import "@/styles/fonts.scss";
 import "@/styles/transitions.scss";
 import "@/styles/main.scss";
-import router from "./router";
 
 createApp(App)
+  .use(store)
   .use(router)
   .mount("#app");
---
+++ src/plugins/store/index.ts
@@ -0,0 +1,15 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    appVersion: "0.2.0",
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
+});
