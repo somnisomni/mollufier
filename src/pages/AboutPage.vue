4a23eba1e914faea44cbe15b35fb3da9c5f3bf5e
somni <me@somni.one>
2021-11-18 00:23:59 +0900

    Add description to noun length limit ignoration option

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about-container">
     <h1>몰?루파이어 <small>v{{ $store.state.appVersion }}</small></h1>
-    <p>여기도 준비중입니당!</p>
+    <p>준비중입니당!</p>
     <p>Made by <a href="https://twitter.com/somni_somni" target="_blank">somni (@somni_somni)</a></p>
   </div>
 </template>
--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="settings-container">
-    <label><input v-model="ignoreNounLengthLimit" type="checkbox" /> 명사 길이 제한 무시</label>
+    <label>
+      <input v-model="ignoreNounLengthLimit" type="checkbox" />
+      명사 길이 제한 무시<br>
+      <small>세 글자 이상의 명사도 몰?루화합니다.</small>
+    </label>
 
     <div><br><br>더 많은 설정이 구현될 예정입니다!</div>
   </div>
