7879259f5cd8dbae80c54a3097c97202087135c7
Gabriel-Dropout <88845385+Gabriel-Dropout@users.noreply.github.com>
2021-11-21 01:52:29 +0900

    Made "mollufy.py" work simple

--- backend/mollufy.py
+++ backend/mollufy.py
@@ -2,25 +2,12 @@ from kiwi_init import initKiwi
 kiwi = initKiwi()
 
 def mollufy(sentence, noLenLimit=False):
-  analyzed = kiwi.analyze(sentence)
-  mollufiedSentence = ""
+  analyzed = kiwi.analyze(sentence)[0][0]
+  mollufiedSentence = sentence
 
-  offset = 0
-
-  for token in analyzed[0][0]:
+  for token in reversed(analyzed):
     if token.tag.startswith("N") and ((noLenLimit and token.len >= 2) or token.len == 2):
-      if offset < token.start:
-        mollufiedSentence += sentence[offset:token.start]
-
-      startOffset = token.start
-      leftEndOffset = startOffset + (token.len - 1)
-      rightEndOffset = leftEndOffset + 1
-
-      mollufiedSentence += "{}?{}".format(sentence[startOffset:leftEndOffset], sentence[leftEndOffset:rightEndOffset])
-
-      offset = token.start + token.len
-  
-  if offset < len(sentence):
-    mollufiedSentence += sentence[offset:]
+      leftEndPos = token.start + (token.len - 1)
+      mollufiedSentence=mollufiedSentence[:leftEndPos] + '?' + mollufiedSentence[leftEndPos:]
 
   return mollufiedSentence
