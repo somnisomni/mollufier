70171db3db09e7c066338867d8a9a217093a9813
somni <me@somni.one>
2021-11-23 21:50:16 +0900

    More `~루` character NNP dics

--- backend/kiwi_init.py
+++ backend/kiwi_init.py
@@ -4,6 +4,9 @@ def initKiwi():
   kiwi = Kiwi()
 
   kiwi.add_user_word("몰루", "NNP")
+  kiwi.add_user_word("아루", "NNP")
+  kiwi.add_user_word("네루", "NNP")
+  kiwi.add_user_word("코하루", "NNP")
   kiwi.add_user_word("아카이브", "NNG")
 
   return kiwi
