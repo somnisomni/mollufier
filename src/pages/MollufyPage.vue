c31ad9ef95e2ddf610cf5beb07bfc4663e1ea82c
somni <me@somni.one>
2021-11-24 17:49:21 +0900

    Implement clear chat history

--- src/pages/MollufyPage.vue
+++ src/pages/MollufyPage.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="chat-container"
         ref="chatContainer">
+    <div v-if="chats.length === 0"
+         style="margin-top: 1rem; text-align: center; opacity: 0.5;">몰?루화한 문장 내역이 여기에 표시됩니다.</div>
+
     <transition-group name="slide-up">
       <chat-item v-for="chat in chats"
                  :key="chat.hash"
@@ -9,11 +12,18 @@
   </div>
 
   <div class="input-container">
-    <input v-model="sentenceToMollu"
-           @keydown.ctrl.enter.exact="doMollufy"
-           type="text"
-           placeholder="몰?루화할 문장 입력..."
-           autofocus />
+    <div class="input-area-wrapper">
+      <button title="채팅 내역 비우기"
+              @click="clearChats">
+        <img src="@/assets/images/mdi-delete.svg" />
+      </button>
+
+      <input v-model="sentenceToMollu"
+             @keydown.ctrl.enter.exact="doMollufy"
+             type="text"
+             placeholder="몰?루화할 문장 입력..."
+             autofocus />
+    </div>
 
     <button @click="doMollufy">몰?루화!</button>
   </div>
@@ -83,6 +93,10 @@ export default class MollufyPage extends Vue {
       /* SCROLL CHAT CONTAINER TO BOTTOM */
       setTimeout(this.chatContainerScrollToEnd, 50);
     }
+  }
+
+  clearChats(): void {
+    this.chats = [];
   }
 
   chatContainerScrollToEnd(): void {
