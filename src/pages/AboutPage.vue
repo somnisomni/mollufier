483d7a6088091b66b98f697659dd1bd6787d79a9
somni <me@somni.one>
2021-12-03 19:10:25 +0900

    Koharu

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -2,6 +2,7 @@
   <div class="about-container">
     <h1>몰?루파이어 <small>v{{ $store.state.appVersion }}</small></h1>
     <p>명사 사이에 물음표를 넣어주어 문장을 "몰?루화"해줍니다.</p>
+    <p style="color: gray"><s>코하루 최고! 코하루는 순수해!</s></p>
     <br />
     <p>Made by <a href="https://twitter.com/somni_somni" target="_blank">somni (@somni_somni)</a></p>
     <p>지능형 한국어 형태소 분석기 <a href="https://github.com/bab2min/Kiwi">Kiwi</a> 사용</p>
