2cf9a4de1ff1f066f460e853fe85638b62a16b71
somni <me@somni.one>
2021-12-03 19:03:15 +0900

    Add simple description of app

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="about-container">
     <h1>몰?루파이어 <small>v{{ $store.state.appVersion }}</small></h1>
-    <p>준비중입니당!</p>
+    <p>명사 사이에 물음표를 넣어주어 문장을 "몰?루화"해줍니다.</p>
+    <br />
     <p>Made by <a href="https://twitter.com/somni_somni" target="_blank">somni (@somni_somni)</a></p>
     <p>지능형 한국어 형태소 분석기 <a href="https://github.com/bab2min/Kiwi">Kiwi</a> 사용</p>
     <p>소스 코드 : <a href="https://github.com/somnisomni/mollufier">GitHub</a></p>
