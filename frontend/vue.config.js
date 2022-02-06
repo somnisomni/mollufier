58325543f827a8cfb877cc61b63c9b1af6ce5b42
somni <me@somni.one>
2022-02-06 22:11:38 +0900

    Set `publicPath` from envvar if available

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,5 +4,6 @@ module.exports = require("@vue/cli-service").defineConfig({
   devServer: {
     allowedHosts: process.env.ALLOWED_HOST ? [process.env.ALLOWED_HOST] : undefined,
   },
+  publicPath: process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : "/",
   transpileDependencies: true,
 });
