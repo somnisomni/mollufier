32b3e4cf7a194a7d11c5db441e11d06505c1d3a5
somni <me@somni.one>
2022-06-08 00:51:41 +0900

    chore

--- container-entrypoint.sh
+++ container-entrypoint.sh
@@ -7,7 +7,7 @@ npx http-server --port 8080 -P http://localhost:8080? ./frontend &
 # Start the backend
 FLASK_PORT=8081 FLASK_HOST=0.0.0.0 python3 ./backend/index.py &
 
-# Wait for processes (remaining running status)
+# Wait for processes (to keep background processes running)
 wait -n
 
 # Exit with errorcode
