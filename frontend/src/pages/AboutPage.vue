f0ce76787af7cf2ab301ca8b0f15057461f00180
somni <me@somni.one>
2022-06-08 01:40:21 +0900

    NAT GAMES... to NEXON GAMES

--- frontend/src/pages/AboutPage.vue
+++ frontend/src/pages/AboutPage.vue
@@ -8,14 +8,12 @@
     <p>지능형 한국어 형태소 분석기 <a href="https://github.com/bab2min/Kiwi">Kiwi</a> 사용</p>
     <p>소스 코드 : <a href="https://github.com/somnisomni/mollufier">GitHub</a></p>
     <p><a href="https://www.pixiv.net/artworks/89423246">블루아카콘</a> by <a href="https://www.pixiv.net/users/64509815" target="_blank">coconutcorn</a></p>
-    <p>배경 이미지 from 게임 "블루아카이브"<br><small>Copyright ⓒ NAT GAMES Co., Ltd.</small></p>
+    <p>배경 이미지 from 게임 "블루아카이브"<br><small>Copyright ⓒ NEXON GAMES Co., Ltd.</small></p>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-class-component";
 
-export default class AboutPage extends Vue {
-
-}
+export default class AboutPage extends Vue { }
 </script>
