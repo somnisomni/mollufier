b9677406aee30530b821df0384c6ea94bd071202
somni <me@somni.one>
2022-06-08 22:30:42 +0900

    Print word if not added in Kiwi custom dict

--- backend/kiwi_init.py
+++ backend/kiwi_init.py
@@ -1,12 +1,22 @@
 from kiwipiepy import Kiwi
+import sys
+import os
+
+def addWord(kiwi: Kiwi, word: str, tag: str, score: float = 0):
+  result = kiwi.add_user_word(word, tag, score)
+
+  if not result:
+    sys.stderr.write("Word '{}' not added to Kiwi model".format(word))
+    sys.stderr.write(os.linesep)
+
 
 def initKiwi():
   kiwi = Kiwi()
 
-  kiwi.add_user_word("몰루", "NNP")
-  kiwi.add_user_word("아루", "NNP")
-  kiwi.add_user_word("네루", "NNP")
-  kiwi.add_user_word("코하루", "NNP")
-  kiwi.add_user_word("아카이브", "NNG")
+  addWord(kiwi, "몰루", "NNP")
+  addWord(kiwi, "아루", "NNP")
+  addWord(kiwi, "네루", "NNP")
+  addWord(kiwi, "코하루", "NNP")
+  addWord(kiwi, "아카이브", "NNG")
 
   return kiwi
