c3c57bacd2abcb5682882b650bd71bd2d9fecf07
somni <me@somni.one>
2024-01-02 02:03:35 +0900

    Fix checkbox clickable area in settings page

--- frontend/src/pages/SettingsPage.vue
+++ frontend/src/pages/SettingsPage.vue
@@ -1,34 +1,32 @@
 <template>
   <div class="settings-container">
     <div class="settings-items-container">
-      <label for="ignoreNounLengthLimit">
+      <h3 style="margin-block-start: 0.5em">설정은 자동으로 저장됩니다.</h3>
+
+      <label>
         <input v-model="ignoreNounLengthLimit"
-               type="checkbox"
-               name="ignoreNounLengthLimit" />
+               type="checkbox">
         명사 길이 제한 무시<br>
         <small>세 글자 이상의 명사도 몰?루화합니다.</small>
       </label>
 
-      <label for="changeMolluMark">
+      <label>
         <input v-model="changeMolluMark"
-               type="checkbox"
-               name="changeMolluMark" />
+               type="checkbox">
         삽입될 기호를 느낌표(!)로 변경<br>
         <small>몰?루 대신 아!루</small>
       </label>
 
-      <label for="forceMollufyForPredefinedWords">
+      <label>
         <input v-model="forceMollufyForPredefinedWords"
-               type="checkbox"
-               name="forceMollufyForPredefinedWords" />
+               type="checkbox">
         일부 단어 강제 몰?루화<br>
         <small>문장에 "몰루"와 같이 잘 알려진 밈 단어가 발견되면 "몰?루"와 같이 무조건 치환됩니다.</small>
       </label>
 
-      <label for="enableMolluImageAnimation">
+      <label>
         <input v-model="enableMolluImageAnimation"
-               type="checkbox"
-               name="enableMolluImageAnimation" />
+               type="checkbox">
         몰?루 배경 일러스트 애니메이션 활성화<br>
         <small>"정말 꼴받게 하네요~"</small>
       </label>
@@ -78,6 +76,8 @@ export default class SettingsPage extends Vue {
 
 <style lang="scss" scoped>
 .settings-container {
+  line-height: 1.25;
+
   .settings-items-container {
     display: flex;
     flex-direction: column;
